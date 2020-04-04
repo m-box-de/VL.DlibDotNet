72fa2f93efa203bdc8bc51a7cff9d5b7a7cbda26
Alexander Shaliapin <alexander@m-box.de>
2020-04-04 15:50:20 +0200

    init

---
+++ src/VL.DlibDotNet/DnnMmodDetectoer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using OpenCvSharp;
+using DlibDotNet;
+using DlibDotNet.Dnn;
+
+namespace VL.DlibDotNet
+{
+    public class DnnMmodDetectoer : IDisposable
+    {
+        LossMmod Detector;
+
+        public DnnMmodDetectoer(string Model)
+        {
+            try
+            {
+                Detector = LossMmod.Deserialize(Model);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException.Message);
+            }
+        }
+
+        public void Update(Mat Image, out IList<Rectangle> Rectangles)
+        {
+            Rectangles = new List<Rectangle>();
+
+            try
+            {
+                if (Image.Empty())
+                {
+                    return;
+                }
+
+                var array = new byte[Image.Width * Image.Height * Image.ElemSize()];
+                Marshal.Copy(Image.CvtColor(ColorConversionCodes.BGR2RGB).Data, array, 0, array.Length);
+
+                using (var img = new Matrix<RgbPixel>(array, Image.Height, Image.Width, Image.ElemSize()))
+                {
+                    // Upsampling the image will allow us to detect smaller faces but will cause the
+                    // program to use more RAM and run longer.
+                    //while (img.Size < 1800 * 1800)
+                    //if (PiramidUp)
+                    //      Dlib.PyramidUp(img);
+
+                    // Note that you can process a bunch of images in a std::vector at once and it runs
+                    // much faster, since this will form mini-batches of images and therefore get
+                    // better parallelism out of your GPU hardware.  However, all the images must be
+                    // the same size.  To avoid this requirement on images being the same size we
+                    // process them individually in this example.
+                    using (var dets = Detector.Operator(img))
+                        foreach (var det in dets)
+                        {
+                            foreach (var d in det)
+                                Rectangles.Add(d.Rect);
+                        }
+
+                    //Console.WriteLine("Hit enter to process the next image.");
+                    //Console.ReadKey();
+                }
+            }
+            catch (CudaException e)
+            {
+               // Console.WriteLine(e.Message);
+                Console.WriteLine(e.DllName);
+                Console.WriteLine(e.DriverVersion);
+                Console.WriteLine(e.ErrorCode);
+                Console.WriteLine(e.ErrorName);
+                Console.WriteLine(e.ErrorMessage);
+                Console.WriteLine(e.RuntimeVersion);
+            }
+        }
+
+        public void Dispose()
+        {
+            Detector.Dispose();
+        }
+    }
+}
---
+++ src/VL.DlibDotNet/VideoTracker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using OpenCvSharp;
+using DlibDotNet;
+
+namespace VL.DlibDotNet
+{
+    public class VideoTracker : IDisposable
+    {
+        CorrelationTracker tracker;
+
+        public VideoTracker()
+        {
+            tracker = new CorrelationTracker();
+        }
+
+        public void Update(Mat Image, DRectangle DRectangle, out DRectangle dRectangle, bool SetTracker = false)
+        {
+            dRectangle = new DRectangle();
+            try
+            {
+                if (Image.Empty())
+                {
+                    return;
+                }
+
+                var array = new byte[Image.Width * Image.Height * Image.ElemSize()];
+                Marshal.Copy(Image.Data, array, 0, array.Length);
+
+                if (SetTracker && !DRectangle.IsEmpty)
+                {
+                    using (var img = Dlib.LoadImageData<BgrPixel>(array, (uint)Image.Height, (uint)Image.Width, (uint)(Image.Width * Image.ElemSize())))
+                    {
+                        tracker.StartTrack(img, DRectangle);
+                    }
+                }
+                else
+                {
+                    if (tracker.GetPosition().IsEmpty)
+                        return;
+
+                    using (var img = Dlib.LoadImageData<BgrPixel>(array, (uint)Image.Height, (uint)Image.Width, (uint)(Image.Width * Image.ElemSize())))
+                    {
+                        tracker.Update(img);
+                        dRectangle = tracker.GetPosition();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Dispose()
+        {
+            tracker.Dispose();
+        }
+    }
+}
